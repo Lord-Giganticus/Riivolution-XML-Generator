b6192af0b028b86a459a53eaff9663ba9cd43f69
Lord-Giganticus <mrubins64@gmail.com>
2021-03-05 15:56:34 -0600

    Add command line version

---
+++ Riivolution XML Generator Command Line/Classes/XML-Generator.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Riivolution_XML_Generator.Classes
+{
+    /// <summary>
+    /// Class for generating the .xml file.
+    /// </summary>
+    class XML_Generator
+    {
+        /// <summary>
+        /// Generates the xml file.
+        /// </summary>
+        /// <param name="Game_ID">The Game ID</param>
+        /// <param name="RGN">Riivolution Page Name.</param>
+        /// <param name="opiton_name">The name of the option</param>
+        /// <param name="PID">The name of the Patch ID</param>
+        /// <param name="fp">The path to the folder containing the mods. (Releative to root:\Riivolution)</param>
+        /// <param name="fn">The path\name of the file.</param>
+        public static void Generate(string Game_ID, string RGN, string opiton_name, string PID, string fp, string fn)
+        {
+            string[] lines =
+            {
+                "<wiidisc version=\"1\">",
+                "\t<id game=\""+Game_ID+"\" />",
+                "\t<options>",
+                "\t\t<section name=\""+RGN+"\">",
+                "\t\t\t<option name=\""+opiton_name+"\">",
+                "\t\t\t\t<choice name=\"Enabled\">",
+                "\t\t\t\t\t<patch id=\""+PID+"\" />",
+                "\t\t\t\t</choice>",
+                "\t\t\t</option>",
+                "\t\t</section>",
+                "\t</options>",
+                "\t<patch id=\""+PID+"\">",
+                "\t<folder external=\"/"+fp+"\" recursive=\"false\" />",
+                "\t\t<folder external=\"/"+fp+"\" disc=\"/\" />",
+                "\t</patch>",
+                "</wiidisc>"
+            };
+            if (!fn.EndsWith(".xml"))
+            {
+                fn = fn + ".xml";
+            }
+            string fileName = fn;
+            File.WriteAllLines(fileName, lines, Encoding.UTF8);
+            return;
+        }
+    }
+}
---
+++ Riivolution XML Generator Command Line/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Riivolution_XML_Generator_Command_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string test = args[5];
+            } catch (Exception err)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(err);
+                return;
+            }
+            Riivolution_XML_Generator.Classes.XML_Generator.Generate(args[0],args[1],args[2],args[3],args[4],args[5]);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Complete. Press enter to exit.");
+            Console.ReadKey();
+        }
+    }
+}
