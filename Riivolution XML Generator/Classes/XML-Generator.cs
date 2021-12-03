26088894bb3094bf44cec549e243cac687b3f77a
IonicPixels <72478475+IonicPixels@users.noreply.github.com>
2021-12-02 20:02:44 -0500

    Started really changing the UI

--- Riivolution XML Generator/Classes/XML-Generator.cs
+++ Riivolution XML Generator/Classes/XML-Generator.cs
@@ -17,11 +17,11 @@ namespace Riivolution_XML_Generator.Classes
         /// <param name="opiton_name">The name of the option</param>
         /// <param name="PID">The name of the Patch ID</param>
         /// <param name="fp">The path to the folder containing the mods.</param>
-        public static void Generate(string Game_ID, string RGN, string opiton_name, string PID, string fp)
+        public static void Generate(string Game_ID, string Localize, string opiton_name, string PatchID, string FolderPath, string Region, string Section_Name, string Choice_Name, string[] comboBox1, string[] comboBox2, string[] comboBox3, string[] comboBox4, string[] comboBox5, string[] comboBox6)
         {
-            if (fp.StartsWith("/") == false)
+            if (FolderPath.StartsWith("/") == false)
             {
-                fp = "/" + fp;
+                FolderPath = "/" + FolderPath;
             } else
             {
                 //pass
@@ -31,17 +31,31 @@ namespace Riivolution_XML_Generator.Classes
                 "<wiidisc version=\"1\">",
                 $"\t<id game=\"{Game_ID}\" />",
                 "\t<options>",
-                $"\t\t<section name=\"{RGN}\">",
+                $"\t\t<section name=\"{Section_Name}\">",
                 $"\t\t\t<option name=\"{opiton_name}\">",
-                "\t\t\t\t<choice name=\"Enabled\">",
-                $"\t\t\t\t\t<patch id=\"{PID}\" />",
+                $"\t\t\t\t<choice name=\"{Choice_Name}\">",
+                $"\t\t\t\t\t<patch id=\"{PatchID}\" />",
                 "\t\t\t\t</choice>",
                 "\t\t\t</option>",
                 "\t\t</section>",
                 "\t</options>",
-                $"\t<patch id=\"{PID}\">",
-                $"\t<folder external=\"{fp}\" recursive=\"false\" />",
-                $"\t\t<folder external=\"{fp}\" disc=\"/\" />",
+                $"\t<patch id=\"{PatchID}\" root=\"{FolderPath}\">",
+                "\t<savegame clone=\"false\" external=\"SaveGame/{$__gameid}{$__region}{$__maker}\"/>",
+                $"<folder external=\"/{PatchID}/ObjectData\" disc=\"/ObjectData\" recursive=\"true\" create=\"true\" />",
+                $"<folder external=\"/{PatchID}/StageData\" disc=\"/StageData\" recursive=\"true\" create=\"true\" />",
+                $"<folder external=\"/{PatchID}/LayoutData\" disc=\"/LayoutData\" recursive=\"true\" create=\"true\" />",
+                $"<folder external=\"/{PatchID}/AudioRes\" disc=\"/AudioRes\" recursive=\"true\" create=\"true\" />",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/UsEnglish\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuFrench\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuSpanish\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuItalian\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuDutch\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuGerman\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/UsFrench\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/UsSpanish\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/EuEnglish\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/JpJapanese\"/>",
+                $"<folder external=\"/{Localize}/UsEnglish\" create=\"true\" recursive=\"true\" disc=\"/KrKorean\"/>",
                 "\t</patch>",
                 "</wiidisc>"
             };
